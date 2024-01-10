9b6c257abeafa3935c5022c27bebe158a14a66b8
kh0kamoni <131918892+kh0kamoni@users.noreply.github.com>
2024-01-10 19:35:45 +0600

    Update bkash.py

--- task7/bkash.py
+++ task7/bkash.py
@@ -8,8 +8,18 @@ while True:
                 number = input("Enter Merchant bKash Account No: ")
                 if len(number) == 11:
                     amount = int(input("Enter Amount: "))
+                    if amount > 0:
+                         reference = int(input("Enter Reference: "))
+                         if reference >= 0:
+                              counter = int(input("Counter: "))
+                              if counter >= 0:
+                                   print(f"Payment\n To: {number}\n Amount: Tk {amount}\n Reference: {reference}\n Counter: {counter}")
+                                   pin = int(input("Enter Menu PIN to Confirm: "))
+                                   print(f"Payment\n Tk {amount}\n to: {number}\n Ref {reference}\n Counter: {counter}. Fee Tk 0.00. Balance Tk XXXXXX. TxrID SKJ33H39 at 15/09/2018 15:12")
+                                   break
+                         
                 else:
-                    print("Invalid number.")
+                        print("Invalid number.")
             else:
                 break
     elif dial == '0':
